7f05cba7becd9ee2b802462cba5d9d9296307def
a100 kh <hatakeyama.k.ac@m.titech.ac.jp>
2024-09-26 15:31:14 +0900

    server

---
+++ 0921tweet/0926talk_server.py
@@ -0,0 +1,50 @@
+
+"""
+
+python -m vllm.entrypoints.openai.api_server \
+    --model weblab-GENIAC/Tanuki-8B-dpo-v1.0 \
+    --enable-lora \
+    --max-lora-rank 128\
+    --lora-modules lora=/data/2024_llm/sftlab/out_data/sftlab-experiments/0923twitter/1-tanuki8b_lora_twitter-zero1/checkpoint-7851
+
+"""
+
+from openai import OpenAI
+
+# Modify OpenAI's API key and API base to use vLLM's API server.
+# openai_api_base = "http://localhost:8080/v1"
+openai_api_base = "http://0.0.0.0:8000/v1"
+client = OpenAI(
+    # api_key=openai_api_key,
+    base_url=openai_api_base,
+)
+
+# model_name="AXCXEPT/EZO-Common-9B-gemma-2-it"
+model_name = "weblab-GENIAC/Tanuki-8B-dpo-v1.0"
+model_name = "lora"
+
+messages = [
+]
+
+while True:
+    q = input("user:")
+
+    if q == "reset":
+        print("reset conversation----")
+        messages = []
+        continue
+
+    # print("user: ", q)
+    messages.append({"role": "user", "content": q})
+    completion = client.chat.completions.create(model=model_name,
+                                                messages=messages,
+                                                temperature=0.3,
+                                                max_tokens=1024,
+                                                # skip_special_tokens=False,
+                                                # repetition_penalty=1.1,
+                                                frequency_penalty=1.1,
+                                                stop="### 指示:",
+                                                )
+    out = completion.choices[0].message.content.strip()
+    print("assistant: ", out)
+    messages.append({"role": "assistant", "content": out})
