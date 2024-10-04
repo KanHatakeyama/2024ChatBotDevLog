0f786fce58b84ff1be6ceff7123a96a83d3aea70
a100 kh <hatakeyama.k.ac@m.titech.ac.jp>
2024-10-04 11:09:11 +0900

    gemma

--- 0921tweet/0926talk_server.py
+++ 0921tweet/0926talk_server.py
@@ -1,15 +1,26 @@
 
 """
-
+tanuki
 python -m vllm.entrypoints.openai.api_server \
     --model weblab-GENIAC/Tanuki-8B-dpo-v1.0 \
     --enable-lora \
     --max-lora-rank 128\
     --lora-modules lora=/data/2024_llm/sftlab/out_data/sftlab-experiments/0923twitter/1-tanuki8b_lora_twitter-zero1/checkpoint-7851
 
+gemma
+python -m vllm.entrypoints.openai.api_server \
+    --model AXCXEPT/EZO-Common-9B-gemma-2-it \
+    --enable-lora \
+    --max-lora-rank 256\
+    --lora-modules lora=/data/2024_llm/sftlab/out_data/sftlab-experiments/0923twitter/1-gemma_lora_twitter_lora-zero1/checkpoint-6870
+
+
 """
 
 from openai import OpenAI
+stop = "### 指示:"
+stop = "<end_of_turn>"
+
 
 # Modify OpenAI's API key and API base to use vLLM's API server.
 # openai_api_base = "http://localhost:8080/v1"
@@ -43,7 +54,7 @@ while True:
                                                 # skip_special_tokens=False,
                                                 # repetition_penalty=1.1,
                                                 frequency_penalty=1.1,
-                                                stop="### 指示:",
+                                                stop=stop,
                                                 )
     out = completion.choices[0].message.content.strip()
     print("assistant: ", out)
